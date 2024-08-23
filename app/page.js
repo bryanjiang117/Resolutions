4939547fd9f430879386c81ecf17ce6e00995834
bryanjiang117 <haozhoujiang@gmail.com>
2024-08-23 19:08:00 -0400

    fix: attempt to fix production 14

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 import { useEffect } from 'react';
 import styles from './styles.module.css';
 import { useList } from '/contexts/ListContext';
