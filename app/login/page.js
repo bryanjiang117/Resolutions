5f79de6c813e5997c2beb0f70ad816bfaded4fc0
bryanjiang117 <haozhoujiang@gmail.com>
2024-08-26 20:53:26 -0400

    chore: color of checkboxes can be primary now (before, the primary foreground and background were the same)

--- app/login/page.js
+++ app/login/page.js
@@ -62,7 +62,6 @@ export default function Login() {
             isSelected={rememberMe}
             onChange={() => setRememberMe((prevState) => !prevState)}
             classNames={{label: "text-small"}}
-            color="secondary"
           >
             Remember me
           </Checkbox>
