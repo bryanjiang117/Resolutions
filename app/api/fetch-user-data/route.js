83f9b3117afd2ddb132422fd974ead4d6aae00cc
bryanjiang117 <haozhoujiang@gmail.com>
2024-08-31 18:10:12 -0400

    feat: endpoint for getting user data

--- app/api/fetch-user-data/route.js
+++ app/api/fetch-user-data/route.js
@@ -1,11 +1,23 @@
-// import { NextResponse } from "next/server";
-// import { sql } from "@vercel/postgres";
-// import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
 
-// export async function GET(request) {
-//   try 
-//   {
-//     const url = new URL(request.url);
-//     const resolution_id = url.searchParams.get('email');
-//   }
-// }
+export async function GET(request) {
+  try 
+  {
+    const url = new URL(request.url);
+    const email = url.searchParams.get('email');
+
+    const response = await sql`
+    SELECT *
+    FROM users
+    WHERE email = ${email};`;
+    
+    const data = response.rows;
+
+    return NextResponse.json({ data }, {status: 200});
+  } catch(error)
+  {
+    console.log(error);
+    return NextResponse.json({ error }, {status: 500});
+  }
+}
